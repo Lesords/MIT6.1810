78faf2db439f83cc6fff25b878ab40af4326cc6b
Lesords <2385342343@qq.com>
2024-01-30 10:44:10 +0800

    feat: add a function to print page table information

--- lab-pgtbl/kernel/vm.c
+++ lab-pgtbl/kernel/vm.c
@@ -449,3 +449,28 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void printpage(pagetable_t pagetable, int depth) {
+  // there are 2^9 = 512 PTEs in a page table.
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if ((pte & PTE_V) == 0) continue;
+
+    for (int j = 0; j < depth; j++) {
+      printf(" ..");
+    }
+    uint64 child = PTE2PA(pte);
+    printf("%d: pte %p pa %p\n", i, pte, child);
+
+    if ((pte & (PTE_R|PTE_W|PTE_X)) == 0) {
+      printpage((pagetable_t) child, depth + 1);
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  printpage(pagetable, 1);
+}
