cead21b9f0d2456264e093df5ea2672e63342b1e
Lesords <2385342343@qq.com>
2024-02-19 15:53:21 +0800

    perf: use a mutex lock to avoid data conflicts caused by multi-threading

--- lab-thread/notxv6/ph.c
+++ lab-thread/notxv6/ph.c
@@ -16,6 +16,7 @@ struct entry {
 struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
+pthread_mutex_t lock[NBUCKET];
 
 
 double
@@ -41,12 +42,15 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&lock[i]);
+
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key)
       break;
   }
+
   if(e){
     // update the existing key.
     e->value = value;
@@ -55,6 +59,7 @@ void put(int key, int value)
     insert(key, value, &table[i], table[i]);
   }
 
+  pthread_mutex_unlock(&lock[i]);
 }
 
 static struct entry*
@@ -116,6 +121,10 @@ main(int argc, char *argv[])
   assert(NKEYS % nthread == 0);
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
+  }
+
+  for (int i = 0; i < NBUCKET; i++) {
+      pthread_mutex_init(&lock[i], NULL);
   }
 
   //
